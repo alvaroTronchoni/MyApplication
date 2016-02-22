e80bcdfaeb280c01412c1168265eea9e03dd3ba0
alvaro <alvaroroto@hotmail.com>
2016-02-22 18:01:33 +0100

    Initial commit

---
+++ app/src/main/java/com/examen/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.examen.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //asdjklfllk
+    }
+}
